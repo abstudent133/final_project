3210cb5daaccc2886971a13aaf7ea19aa86dd630
IReallyReallyLikeMangoes25 <mirai.hsiao@ucas-edu.net>
2026-04-21 09:50:17 -0600

    pseudocode

---
+++ src/csv_management.py
@@ -0,0 +1,13 @@
+# Mh 1st csv management pseudocode
+
+# save csv, takes in user dictionary:
+    # gets all keys and values
+    # loops over them and adds them all to a list
+    # converts the list to a dataframe
+    # adds dataframe to the csv
+
+
+# load csv:
+    # create dataframe
+    # loops over rows in the csv and add them to a dictionary
+    # return the dictionary
---
+++ src/work.py
@@ -0,0 +1,10 @@
+# MH 1st work function
+
+# work, takes in user dictionary:
+    # loads customers csv as dictionary
+    # randomly selects customer from the dictionary and displays their request and sprite
+    # allows user to select a response
+    # if the user chooses a response saved as good they get a high tip
+    # if the user selects a response saved as bad they get a bad tip or none at all
+    # adds the tip to the users total money
+    # returns updated dictionary
