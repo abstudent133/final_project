25d6e713bd137291684874cfc300e958cce31905
abstudent133 <anna.bangerter@ucas-edu.net>
2026-04-23 09:53:12 -0600

    started code for the user class

--- src/classes.py
+++ src/classes.py
@@ -2,16 +2,26 @@
 #import random
 
 #User class
+class User():
     #initiate(username, password)
+    def __init__(self, username):
+        self.username = username
     #formate to add
+    def formate_dict(self):
         #take username, hashed password, starting amount of money, and an empty invintory and create a dictionary
         #return it
-    #string
-        #take the username, hashed password, and starting amount of money and formate it as a string to print
-        #return it
+        avatar_base = None
+        user = {"username": self.username,
+                "password": self.password(),
+                "money": 100,
+                "avatar base": avatar_base,
+                "inventory": {}                
+}
     #password
+    def password(self):
         #ask for the password
         #take the password, hash it
+        pass
 
     
 
