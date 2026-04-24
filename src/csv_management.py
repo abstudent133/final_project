ad2f744a96584f84da3824ed94709ddf38bb05be
IReallyReallyLikeMangoes25 <mirai.hsiao@ucas-edu.net>
2026-04-24 09:51:00 -0600

    more characters

--- src/csv_management.py
+++ src/csv_management.py
@@ -1,16 +1,32 @@
 # Mh 1st csv management pseudocode
+import pandas as pd
+import ast
 
 # save csv, takes in user dictionary:
+def save_df(dictionary, file_path):
     # gets all keys and values
+    keys = list(dictionary.keys())
+    values = list(dictionary.values())
+    row_data = []
     # loops over them and adds them all to a list
+    for i in range(len(keys)):
+        row_data.append([keys[i], values[i]])
     # converts the list to a dataframe
+    df = pd.DataFrame(row_data, columns = ["names", "info"])
     # adds dataframe to the csv
+    df.to_csv(file_path)
 
 
 # load csv:
+def load_df(file_path):
     # create dataframe
+    df = pd.read_csv(file_path)
+    char_dict = {}
     # loops over rows in the csv and add them to a dictionary, saving by username
+    for index, row in df.iterrows():
+        char_dict[row["names"]] = ast.literal_eval(row["info"])
     # return the dictionary
+    return char_dict
 
 # DICTIONARY STRUCTURE:
     # users = {
